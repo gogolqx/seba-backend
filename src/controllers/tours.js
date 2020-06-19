30cd677236dd0a3d9c3b334cc0f6c5d30f35bfef
liu0001q <QiaoXi.Liu@prosiebensat1.com>
2020-06-19 22:08:57 +0200

    added delete and update for tours

--- src/controllers/tours.js
+++ src/controllers/tours.js
@@ -6,7 +6,6 @@ const create = async(req, res) => {
         description: req.body.description
     });
     
-
     tour.save()
         .catch(error => res.status(500).json({
         error: 'Internal server error',
@@ -16,12 +15,27 @@ const create = async(req, res) => {
     res.json(tour);
         
 };
+const update = (req, res) => {
+   /* if (Object.keys(req.body).length === 0)
+    {
+        return res.status(400).json({
+            error: 'Bad Request',
+            message: 'The request body is empty'
+        });
+    }*/
 
-
+    Tour.findByIdAndUpdate(req.params.id,req.body,{
+        new: true,
+        runValidators: true}).exec()
+        .then(tour => res.status(200).json(tour))
+        .catch(error => res.status(500).json({
+            error: 'Internal server error',
+            message: error.message
+        }));
+};
 
 const list  = async(req, res) => {
     console.log('I am here getting tour');
-    
     try{
         const tours = await Tour.find();
         res.json(tours);
@@ -30,12 +44,19 @@ const list  = async(req, res) => {
     }
 
 };
-
+const remove = (req, res) => {
+    Tour.findByIdAndRemove(req.params.id).exec()
+        .then(() => res.status(200).json({message: `Tour with id${req.params.id} was deleted`}))
+        .catch(error => res.status(500).json({
+            error: 'Internal server error',
+            message: error.message
+        }));
+};
 
 module.exports = {
     create,
     //  read,
-    //  update,
-    //  remove,
+    update,
+    remove,
     list
 };
--- src/routes/tours.js
+++ src/routes/tours.js
@@ -7,8 +7,8 @@ const TourController = require('../controllers/tours');
 
 router.get('/', TourController.list); // List all tours
 router.post('/', TourController.create); // Create a new tours   middlewares.checkAuthentication, 
-
-
+router.put('/:id',  TourController.update); // Update a movie by Id   middlewares.checkAuthentication,
+router.delete('/:id',  TourController.remove); // Delete a movie by Id    middlewares.checkAuthentication,
 
 
 module.exports = router;
