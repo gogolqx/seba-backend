aa9fe1b146e7eb64b878067c2257107c53b1a09b
liu0001q <QiaoXi.Liu@prosiebensat1.com>
2020-06-21 23:28:21 +0200

    slightly modified the tour schema and users schema

--- src/models/Tour.js
+++ src/models/Tour.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
 
 const TourSchema = new mongoose.Schema({
+    /*
+    * I make review_id is an array of int(contains review ids)
+    */
+    id: {
+        type: Number,
+        required: true
+    },
+    guide_id: Number,
     title: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    city:{
         type: String,
         required: true
     },
@@ -17,10 +17,23 @@ const TourSchema = new mongoose.Schema({
         type: Date,
         default: Date.now
     },
-    price:{
-        type: Number,
-        default: 0
+    language: String,
+    location: [Number],
+    city:{
+        type: String,
+        required: true
     },
+    description: {
+        type: String,
+        required: true
+    },
+    max_participants: Number,
+    price: Number,
+    time: {
+        type: Date,
+        default: Date.now
+    },
+    review_id: [Number],
     rating:{
         type: Number,
         default: 3
