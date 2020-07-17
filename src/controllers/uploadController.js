de9032eeab77abd4d3b64321b30336d950d9bbc0
liu0001q <QiaoXi.Liu@prosiebensat1.com>
2020-07-17 21:47:04 +0200

    update uploading, added url

--- src/controllers/uploadController.js
+++ src/controllers/uploadController.js
@@ -1,21 +1,58 @@
 const upload = require("../middlewares/multer");
+const path = require("path");
+var Grid = require('gridfs-stream');
+var mongoose = require("mongoose");
+Grid.mongo = mongoose.mongo;
 
 const uploadFile = async (req, res) => {
   try {
     await upload.uploadFilesMiddleware(req, res);
 
     console.log(req.file);
+    
+
     if (req.file == undefined) {
       return res.send(`You must select a file.`);
     }
+    var gfs = new Grid(mongoose.connection.db);
+    await gfs.findOne({ filename: res.filename }, function (err, file) {
+        if (err) {
+            return res.status(400).send(err);
+        }
+        else if (!file) {
+            return res.status(404).send('Error on the database looking for the file.');
+        }
+    
+        res.set('Content-Type', file.contentType);
+        res.set('Content-Disposition', 'attachment; filename="' + file.filename + '"');
+    
+        var readstream = gfs.createReadStream({
+            filename: res.filename 
+        });
+    
+        readstream.on("error", function(err) { 
+            res.end();
+        });
+        readstream.pipe(res);
+      });
+    
 
-    return res.json(req.file);
+
+
+//return res.sendFile();
+    //return res.json(req.file);
   } catch (error) {
     console.log(error);
     return res.send(`Error when trying upload image: ${error}`);
   }
 };
-s
-module.exports = {
+
+
+const home = (req, res) => {
+  return res.sendFile(path.join(`${__dirname}/../views/index.html`));
+};
+
+
+module.exports = {home,
     uploadFile
 };
--- src/routes/toursRoute.js
+++ src/routes/toursRoute.js
@@ -3,13 +3,28 @@ const router = express.Router();
 const middlewares = require('../middlewares/authentication');
 const TourController = require('../controllers/toursController');
 const UploadController = require('../controllers/uploadController');
+var multiparty = require('multiparty');
 //router.post('/upload', busboy(),TourController.upload);
-router.post('/upload', UploadController.uploadFile);
+//router.post('/upload', UploadController.uploadFile);
 router.post('/search', TourController.search);
 router.get('/', TourController.list); // List all tours
 router.post('/:username/create',  middlewares.checkGuideAuthentication,TourController.create); // Create a new tours   middlewares.checkAuthentication, 
 router.put('/:username/:tour_id',   middlewares.checkGuideAuthentication,TourController.update); // Update a tours by Id   middlewares.checkAuthentication,
 router.delete('/:username/:tour_id',  middlewares.checkGuideAuthentication, TourController.remove); // Delete a tours by Id    middlewares.checkAuthentication,
 
+router.post('/upload', function(req, res, next) { 
+    let form = new multiparty.Form(); 
+    var path = require('path'); 
+    form.uploadDir=path.resolve(__dirname,'../../public/image'); 
+    console.log(form.uploadDir)
+    form.keepExtensions=true; 
+    form.parse(req,function(err,fields,files){ //
+        if(err){ 
+            console.log(files); 
+            res.json({ status:"1", msg:"Failed."+err }); }
+        else{ 
+            
+            res.json({ status:"0", msg:"Succuss!", img_url: "http://localhost:3000"+files.file[0].path.split("public")[1] }); } }); });
 
-module.exports = router;
+
+module.exports = router; 
