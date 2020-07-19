0da0a9b6db37ef9a16e95ae2becdd3c75c2af51b
liu0001q <QiaoXi.Liu@prosiebensat1.com>
2020-07-19 13:21:51 +0200

    merged with GuideTour

--- src/controllers/toursController.js
+++ src/controllers/toursController.js
@@ -1,6 +1,4 @@
 const Tour = require('../models/Tour');
-const User = require('../models/User');
-
 nodeGeocoder = require('node-geocoder');
 
 
@@ -8,7 +6,7 @@ const options = {
     provider: 'openstreetmap'
   };
 
-
+ 
 var add_schedule =  function (dt, hours,mins) {
     return new Date(dt.getTime() + hours*60*60*1000+mins*60*1000);
 }
@@ -18,13 +16,12 @@ const geoCoder = nodeGeocoder(options);
 
 // creating tour
 const create = async (req, res) => {
-
-    console.log("your are creating a tour")
+    console.log("your are creating a tour");
     if (Object.keys(req.body).length === 0) return res.status(400).json({
         error: 'Bad Request',
         message: 'The request body is empty'
     });
-
+  
     let new_tour;
     let new_dates=[];
     dates = req.body.dates;
@@ -38,15 +35,15 @@ const create = async (req, res) => {
             new_dates.push(date_time);
         }
     };
-
+    
     for (let booking_d of new_dates) {
-        booking_dates.push({"date":booking_d,"seats":req.body.max_participants});
+        booking_dates.push({"date":booking_d,"seats":req.body.max_participants}); 
     }
-
+    
     // this is the final request
     new_tour = new Tour ({
     title:req.body.title,
-    guide_id:req.userId,
+    guide_id:req.userId,  
     description:req.body.description,
     country: crg.get_country(latitude,longitude),
     city: req.body.city,
@@ -69,7 +66,7 @@ const create = async (req, res) => {
             message: error.message
         }));
 };
-//
+// 
 
 
 
@@ -134,7 +131,7 @@ const search = async(req, res) => {
 
 
 // listing all tours
-//TODO:
+//TODO: 
 const list  = async(req, res) => {
     console.log('I am here getting tour');
     try{
@@ -143,28 +140,8 @@ const list  = async(req, res) => {
     }catch(err){
         res.json({message:err});
     }
+
 };
-
-const guidesTours  = async(req, res) => {
-  console.log(req.body);
-  
-  const user = await User.findOne(
-      {username : req.params.username}
-  ).select().exec();
-  console.log('user: ', user);
-
-  if (user.role === "traveler") return res.status(404).json({
-      error: 'Not Found',
-      message: `User not found`
-  });
-  const tours = await Tour.find(
-    {guide_id:user._id}
-  ).exec();
-
-  console.log('tours: ', tours);
-  res.json(tours);
-};
-
 
 const remove = (req, res) => {
     Tour.findByIdAndRemove(req.params.tour_id).exec()
@@ -175,13 +152,13 @@ const remove = (req, res) => {
         }));
 };
 // updating tour
-//TODO:
+//TODO: 
 const update = (req, res) => {
     if (Object.keys(req.body).length === 0)
     {
         return res.status(400).json({
             error: 'Bad Request',
-            message: 'The request body is empty'
+            message: 'The request body is empty' 
         });
     }
 
@@ -195,6 +172,25 @@ const update = (req, res) => {
             message: error.message
         }));
 };
+const guidesTours  = async(req, res) => {
+    console.log(req.body);
+    
+    const user = await User.findOne(
+        {username : req.params.username}
+    ).select().exec();
+    console.log('user: ', user);
+  
+    if (user.role === "traveler") return res.status(404).json({
+        error: 'Not Found',
+        message: `User not found`
+    });
+    const tours = await Tour.find(
+      {guide_id:user._id}
+    ).exec();
+  
+    console.log('tours: ', tours);
+    res.json(tours);
+  };
 module.exports = {
     create,
     search,
@@ -202,7 +198,7 @@ module.exports = {
     update,
     remove,
     list,
-    guidesTours,
+    guidesTours
 };
 
 /*backup using boy to upload file in local disk
@@ -212,7 +208,7 @@ const upload = async(req,res)=>{
         console.log(req.busboy);
         req.busboy.on('file', function (fieldname, file, filename, encoding, mimetype) {
            var saveTo = path.join(__dirname.replace('routes', 'static'), "yourFileName");
-           console.log(saveTo);
+           console.log(saveTo);   
             file.pipe(fs.createWriteStream(saveTo));
             file.on('end', function () {
                    //database
@@ -223,6 +219,6 @@ const upload = async(req,res)=>{
         });
         req.pipe(req.busboy);
     }
-
+   
 };
 */
